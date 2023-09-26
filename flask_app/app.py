7e4b0a53802abc6009394bf120fa17c7ac57f5a0
captainsalazar <57871522+syedalizain033@users.noreply.github.com>
2023-09-26 14:56:03 +0500

    V1 completed with About page added.

--- flask_app/app.py
+++ flask_app/app.py
@@ -83,7 +83,9 @@ def investigate():
         
     return render_template("investigate.html")
  
- 
+@app.route("/about")
+def about():
+    return render_template("about.html") 
  
 if __name__ == "__main__":
     app.run()
